a2902491100bc6a2a1e0cbf1145fbcb46e208832
Lnx <46533182+lnxAv@users.noreply.github.com>
2023-12-21 10:37:46 -0500

    Removing secrets fallback

--- server/lib/dbConnection.ts
+++ server/lib/dbConnection.ts
@@ -2,10 +2,10 @@ import { drizzle } from "drizzle-orm/mysql2";
 import mysql from "mysql2/promise";
 
 const connection = await mysql.createConnection({
-  user: process.env.DATABASE_USER || "root",
-  password: process.env.DATABASE_PASSWORD || "admin",
-  host: process.env.DATABASE_HOST || "localhost",
-  port: Number(process.env.DATABASE_PORT) || 3306,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  host: process.env.DATABASE_HOST,
+  port: Number(process.env.DATABASE_PORT),
   database: process.env.DATABASE_DATABASE,
 });
 
